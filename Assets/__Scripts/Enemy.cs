12c4e3f56e591bff78d7202e737b9d06a7a68501
DESKTOP\TheBestWorker <thebestworkerinthecountry@gmail.com>
2022-06-28 23:37:34 +0300

    Random item drop after dead enemy

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class Enemy : MonoBehaviour
     public float knockBackSpeed = 10;
     public float knockBackDuration = 0.25f;
     public float invincibleDuration = 0.5f;
+    public GameObject[] randomItemDrops;
     public GameObject guaranteedItemDrop = null;
 
     [Header("Set Dynamically: Enemy")] 
@@ -90,6 +92,16 @@ public class Enemy : MonoBehaviour
         {
             go = Instantiate<GameObject>(guaranteedItemDrop);
             go.transform.position = transform.position;
+        }
+        else if (randomItemDrops.Length > 0)
+        {
+            int n = Random.Range(0, randomItemDrops.Length);
+            GameObject prefab = randomItemDrops[n];
+            if (prefab != null)
+            {
+                go = Instantiate<GameObject>(prefab);
+                go.transform.position = transform.position;
+            }
         }
         Destroy(gameObject);
     }
